3035b1805dc5e1be0f736f5105e5a2e163d11f92
Will Wright <will@clazzoo.com>
2017-05-17 15:51:25 +0100

    Add VideoList to app and pass videos props to it

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDn1XNHgKnv316l-NVhmXDkWjfl6iLKYlY';
 
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }

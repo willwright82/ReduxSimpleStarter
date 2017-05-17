d586f3873fae2e2b1c8e7bdc7d2675fcd975133d
Will Wright <will@clazzoo.com>
2017-05-17 14:51:38 +0100

    Add an API test call to YTSearch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDn1XNHgKnv316l-NVhmXDkWjfl6iLKYlY';
+
+YTSearch({key: API_KEY, term: 'skateboarding'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce some HTML
 const App = () => {

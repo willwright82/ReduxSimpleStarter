b299ef9eca5ff45a4d990b84301d93dac14af4dd
Will Wright <will@clazzoo.com>
2017-05-22 12:19:34 +0100

    Add 'Loading..' while no video loaded

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+  if (!video) {
+    return (
+      <div>Loading...}</div>
+    );
+  }
+
   const videoId = video.id.videoId;
   //const url = 'https://www.youtube.com/embed/' + videoId;
   const url = `https://www.youtube.com/embed/${videoId}`; //ES6 string interpolation
